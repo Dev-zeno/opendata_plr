f61567e9f896cf9f893a827ad90b0c916ff87eee
Zeno <h.kijung06@gmail.com>
2026-03-02 13:59:43 +0900

    화재 정상화

--- api/proxy-reading-room.js
+++ api/proxy-reading-room.js
@@ -7,31 +7,31 @@ module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  
+
   // OPTIONS 요청 처리 (CORS preflight)
   if (req.method === 'OPTIONS') {
     res.status(200).end();
     return;
   }
-  
+
   // GET 요청만 허용
   if (req.method !== 'GET') {
     res.status(405).json({ error: 'Method not allowed' });
     return;
   }
-  
+
   // 1000 줄 변경
   try {
-    const url = `https://opendata.klid.or.kr/hapi/pblib/rlt_rdrm_info?pageNo=1&numOfRows=1000&serviceKey=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJzZXJ2aWNlX2tleSIsImV4cCI6MjA1MjAxNDEwMywidXNlcl9jb2RlIjoiSE9NRSIsInVzZXJfcm9sZSI6InBiZG8ifQ.exJZSk_ABg7Jv3AaS0dsUzOFqT39F8M_gntxZt2EthZM40uqI9-RQtz7HTHd5UeFMF4brphJTYkmXDVg74_YTg`;
-    
+    const url = 'https://apis.data.go.kr/B551982/plr_v2/rlt_rdrm_info_v2?serviceKey=80j%2FK4JyieJVqIy2fT0qM5ziOrx42wpgHUNb%2BKZQOQ8fGYSohlz2aUfwIBnGQYO38KXJ2szvUBa%2FCOX2W95PuQ%3D%3D&pageNo=1&numOfRows=1000&type=json';
+
     const data = await new Promise((resolve, reject) => {
       https.get(url, (response) => {
         let data = '';
-        
+
         response.on('data', (chunk) => {
           data += chunk;
         });
-        
+
         response.on('end', () => {
           try {
             // 응답이 JSON 형식인지 XML 형식인지 확인
@@ -57,7 +57,7 @@ module.exports = async function handler(req, res) {
         reject(error);
       });
     });
-    
+
     res.status(200).json(data);
   } catch (error) {
     console.error('Proxy error for reading room:', error);
